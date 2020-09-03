97344571020086f030f48293afc17abfaf0e8dd0
martucazpo <martucazpo@gmail.com>
2020-09-02 22:11:47 -0700

    login/register modals functional

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,16 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginModalAnchor = document.getElementById("loginModalAnchor");
-    const tic = document.getElementById("tic");
+    const registrationModalAnchor = document.getElementById("registrationModalAnchor");
     const registrationDivModal = document.getElementById("registrationDivModal");
     const loginDivModal = document.getElementById("loginDivModal");
+    const loginNavBtn = document.getElementById("loginNavBtn");
+    const gotoRegisterBtn = document.getElementById("gotoRegisterBtn");
+    const loginBtn = document.getElementById("loginBtn");
+    const registerBtn = document.getElementById("registerBtn");
 
 
-    tic.addEventListener('click', () => {
+    registrationModalAnchor.addEventListener('click', () => {
         showHidden(registrationDivModal, "display", "hidden");
     });
 
     loginModalAnchor.addEventListener('click', () => {
         showHidden(loginDivModal, "display", "hidden");
+    });
+
+    loginNavBtn.addEventListener('click', () => {
+        showHidden(loginDivModal, "hidden", "display");
+    });
+
+    gotoRegisterBtn.addEventListener('click', () => {
+        showHidden(loginDivModal, "display", "hidden");
+        showHidden(registrationDivModal, "hidden", "display");
+    });
+
+    loginBtn.addEventListener('click', () => {
+        showHidden(loginDivModal, "display", "hidden");
+    });
+
+    registerBtn.addEventListener('click', () => {
+        showHidden(registrationDivModal, "display", "hidden");
     });
 });
 
