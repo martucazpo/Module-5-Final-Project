0ff7148130fdd0dbfbf54f10f5e54fd819c5ea41
martucazpo <martucazpo@gmail.com>
2020-09-02 18:50:55 -0700

    registration and login modals

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,1 +1,24 @@
-console.log('hello');
+document.addEventListener('DOMContentLoaded', () => {
+    const loginModalAnchor = document.getElementById("loginModalAnchor");
+    const tic = document.getElementById("tic");
+    const registrationDivModal = document.getElementById("registrationDivModal");
+    const loginDivModal = document.getElementById("loginDivModal");
+
+
+    tic.addEventListener('click', () => {
+        showHidden(registrationDivModal, "display", "hidden");
+    });
+
+    loginModalAnchor.addEventListener('click', () => {
+        showHidden(loginDivModal, "display", "hidden");
+    });
+});
+
+function showHidden(div, targetClass1, targetClass2) {
+    if (div.classList.contains(targetClass1)) {
+        div.classList.remove(targetClass1);
+        div.classList.add(targetClass2);
+    } else {
+        div.setAttribute("Class", targetClass2);
+    }
+}
