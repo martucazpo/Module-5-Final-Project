4ab55b9c938d4e3e769d412e260859d1e6f52a04
martucazpo <martucazpo@gmail.com>
2020-09-07 21:51:17 -0700

    admin ex update

--- routes/site/admin/adminLanding.js
+++ routes/site/admin/adminLanding.js
@@ -3,14 +3,65 @@ const moment = require('moment');
 const User = require('../../../models/User');
 
 router.get('/landing', (req, res) => {
-    User.find( { _id: { $ne: "5f559c8ec14b545a3c860d9b" } }, (err, data) => {
+    User.find({
+        _id: {
+            $ne: "5f559c8ec14b545a3c860d9b"
+        }
+    }, (err, data) => {
         if (err) {
             console.log(err);
         } else {
+            let showLog = "hidden";
             let users = data;
-            res.render('layouts/site/admin-landing.ejs', { users, moment });
+            let exerciseModal = "no";
+            res.render('layouts/site/admin-landing.ejs', {
+                showLog,
+                users,
+                moment,
+                exerciseModal
+            });
         }
-    });  
+    });
 });
+
+router.get('/exercise-log/:id', (req, res) => {
+    let id = req.params.id;
+    User.findById({
+        _id: id
+    }).populate('exercises').exec((err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let name = data.name;
+            let id = data._id;
+            let exercises = data.exercises;
+            User.find({
+                _id: {
+                    $ne: "5f559c8ec14b545a3c860d9b"
+                }
+            }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    let users = data;
+                    let showLog = "display";
+                    let showErrs = "hidden";
+                    let exerciseModal = "yes";
+                    res.render('layouts/site/admin-landing.ejs', {
+                        users,
+                        name,
+                        id,
+                        exercises,
+                        moment,
+                        showLog,
+                        showErrs,
+                        exerciseModal
+                    });
+                }
+            });
+        }
+    });
+});
+
 
 module.exports = router;
